1783eaded57d7ed73fe30d8b22db57458cd2129d
EugenyVinogradov <vinogradov_e_v@rambler.ru>
2022-07-26 18:53:11 +0300

    Create all tests,build failed version2

--- src/test/java/ru/netology/TestCardDelivery.java
+++ src/test/java/ru/netology/TestCardDelivery.java
@@ -10,6 +10,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import org.openqa.selenium.Keys;
+
 import java.text.SimpleDateFormat;
 import java.time.Duration;
 import java.util.Date;
@@ -26,6 +27,7 @@ public class TestCardDelivery {
         open("http://localhost:9999");
         Configuration.headless = true;
     }
+
     SelenideElement form = $("form");
     Date date = new Date();
     SimpleDateFormat d = new SimpleDateFormat("dd.MM.YYYY");
@@ -126,6 +128,7 @@ public class TestCardDelivery {
         $("[data-test-id=date] .input_invalid .input__sub").shouldHave(exactText("Заказ на выбранную дату невозможен"));
         $("[data-test-id=notification]").shouldNotBe(Condition.visible);
     }
+
     @Test
     void shouldSubmitRequestIfDateIsFourDaysFromCurrent() {
         form.$("[data-test-id=city] input").setValue("Нижний Новгород");
@@ -138,6 +141,7 @@ public class TestCardDelivery {
         form.$(".button__text").click();
         $("[data-test-id=notification]").shouldBe(Condition.visible, Duration.ofSeconds(15));
     }
+
     @Test
     void shouldNotSubmitRequestIfDateIsEmpty() {
         form.$("[data-test-id=city] input").setValue("Нижний Новгород");
@@ -151,6 +155,7 @@ public class TestCardDelivery {
         $("[data-test-id=date] .input_invalid .input__sub").shouldHave(exactText("Неверно введена дата"));
         $("[data-test-id=notification]").shouldNotBe(Condition.visible);
     }
+
     @Test
     void shouldNotSubmitRequestIfDayInDateNotExist() {
         form.$("[data-test-id=city] input").setValue("Нижний Новгород");
@@ -164,6 +169,7 @@ public class TestCardDelivery {
         $("[data-test-id=date] .input_invalid .input__sub").shouldHave(exactText("Неверно введена дата"));
         $("[data-test-id=notification]").shouldNotBe(Condition.visible);
     }
+
     @Test
     void shouldNotSubmitRequestIfMonthInDateNotExist() {
         form.$("[data-test-id=city] input").setValue("Нижний Новгород");
@@ -177,19 +183,21 @@ public class TestCardDelivery {
         $("[data-test-id=date] .input_invalid .input__sub").shouldHave(exactText("Неверно введена дата"));
         $("[data-test-id=notification]").shouldNotBe(Condition.visible);
     }
-//    @Test
-//    void shouldNotSubmitRequestIfYearInDateIsTooBig() {
-//        form.$("[data-test-id=city] input").setValue("Нижний Новгород");
-//        form.$("[data-test-id=date] input").sendKeys(Keys.CONTROL + "a");
-//        form.$("[data-test-id=date] input").sendKeys(Keys.DELETE);
-//        form.$("[data-test-id=date] input").setValue("01.12.9999");
-//        form.$("[data-test-id=name] input").setValue("Иванов Иван");
-//        form.$("[data-test-id=phone] input").setValue("+79270000000");
-//        form.$("[data-test-id=agreement]").click();
-//        form.$(".button__text").click();
-//        $("[data-test-id=date] .input_invalid .input__sub").shouldHave(exactText("Неверно введена дата"));
-//        $("[data-test-id=notification]").shouldNotBe(Condition.visible);
-//    }
+
+    @Test
+    void shouldNotSubmitRequestIfYearInDateIsTooBig() {
+        form.$("[data-test-id=city] input").setValue("Нижний Новгород");
+        form.$("[data-test-id=date] input").sendKeys(Keys.CONTROL + "a");
+        form.$("[data-test-id=date] input").sendKeys(Keys.DELETE);
+        form.$("[data-test-id=date] input").setValue("01.12.9999");
+        form.$("[data-test-id=name] input").setValue("Иванов Иван");
+        form.$("[data-test-id=phone] input").setValue("+79270000000");
+        form.$("[data-test-id=agreement]").click();
+        form.$(".button__text").click();
+        $("[data-test-id=date] .input_invalid .input__sub").shouldHave(exactText("Неверно введена дата"));
+        $("[data-test-id=notification]").shouldNotBe(Condition.visible);
+    }
+
     @Test
     void shouldNotSubmitRequestIfYearInDateIsTooSmall() {
         form.$("[data-test-id=city] input").setValue("Нижний Новгород");
@@ -203,17 +211,19 @@ public class TestCardDelivery {
         $("[data-test-id=date] .input_invalid .input__sub").shouldHave(exactText("Заказ на выбранную дату невозможен"));
         $("[data-test-id=notification]").shouldNotBe(Condition.visible);
     }
-//    @Test
-//    void shouldSubmitRequestIfNameIsMoreTwoWords() {
-//        form.$("[data-test-id=city] input").setValue("Москва");
-//        form.$("[data-test-id=date] input").setValue(dateCurrentPlusThreeDays);
-//        form.$("[data-test-id=name] input").setValue("Иванов Иван Иванович");
-//        form.$("[data-test-id=phone] input").setValue("+79033223322");
-//        form.$("[data-test-id=agreement]").click();
-//        form.$(".button__text").click();
-//        $("[data-test-id=name].input_invalid .input__sub").shouldHave(exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
-//        $("[data-test-id=notification]").shouldNotBe(Condition.visible);
-//    }
+
+    @Test
+    void shouldSubmitRequestIfNameIsMoreTwoWords() {
+        form.$("[data-test-id=city] input").setValue("Москва");
+        form.$("[data-test-id=date] input").setValue(dateCurrentPlusThreeDays);
+        form.$("[data-test-id=name] input").setValue("Иванов Иван Иванович");
+        form.$("[data-test-id=phone] input").setValue("+79033223322");
+        form.$("[data-test-id=agreement]").click();
+        form.$(".button__text").click();
+        $("[data-test-id=name].input_invalid .input__sub").shouldHave(exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
+        $("[data-test-id=notification]").shouldNotBe(Condition.visible);
+    }
+
     @Test
     void shouldSubmitRequestIfNameContainDigital() {
         form.$("[data-test-id=city] input").setValue("Москва");
@@ -225,6 +235,7 @@ public class TestCardDelivery {
         $("[data-test-id=name].input_invalid .input__sub").shouldHave(exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
         $("[data-test-id=notification]").shouldNotBe(Condition.visible);
     }
+
     @Test
     void shouldSubmitRequestIfNameContainLatin() {
         form.$("[data-test-id=city] input").setValue("Москва");
@@ -236,6 +247,7 @@ public class TestCardDelivery {
         $("[data-test-id=name].input_invalid .input__sub").shouldHave(exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
         $("[data-test-id=notification]").shouldNotBe(Condition.visible);
     }
+
     @Test
     void shouldSubmitRequestIfNameContainSpecialSymbol() {
         form.$("[data-test-id=city] input").setValue("Москва");
@@ -247,17 +259,19 @@ public class TestCardDelivery {
         $("[data-test-id=name].input_invalid .input__sub").shouldHave(exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
         $("[data-test-id=notification]").shouldNotBe(Condition.visible);
     }
-//    @Test
-//    void shouldSubmitRequestIfNameContainOnlyOneWord() {
-//        form.$("[data-test-id=city] input").setValue("Москва");
-//        form.$("[data-test-id=date] input").setValue(dateCurrentPlusThreeDays);
-//        form.$("[data-test-id=name] input").setValue("Иванов");
-//        form.$("[data-test-id=phone] input").setValue("+79033223322");
-//        form.$("[data-test-id=agreement]").click();
-//        form.$(".button__text").click();
-//        $("[data-test-id=name].input_invalid .input__sub").shouldHave(exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
-//        $("[data-test-id=notification]").shouldNotBe(Condition.visible);
-//    }
+
+    @Test
+    void shouldSubmitRequestIfNameContainOnlyOneWord() {
+        form.$("[data-test-id=city] input").setValue("Москва");
+        form.$("[data-test-id=date] input").setValue(dateCurrentPlusThreeDays);
+        form.$("[data-test-id=name] input").setValue("Иванов");
+        form.$("[data-test-id=phone] input").setValue("+79033223322");
+        form.$("[data-test-id=agreement]").click();
+        form.$(".button__text").click();
+        $("[data-test-id=name].input_invalid .input__sub").shouldHave(exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
+        $("[data-test-id=notification]").shouldNotBe(Condition.visible);
+    }
+
     @Test
     void shouldSubmitRequestIfNameIsEmpty() {
         form.$("[data-test-id=city] input").setValue("Москва");
@@ -269,6 +283,7 @@ public class TestCardDelivery {
         $("[data-test-id=name].input_invalid .input__sub").shouldHave(exactText("Поле обязательно для заполнения"));
         $("[data-test-id=notification]").shouldNotBe(Condition.visible);
     }
+
     @Test
     void shouldSubmitRequestIfNameContainOnlySpaces() {
         form.$("[data-test-id=city] input").setValue("Москва");
@@ -280,6 +295,7 @@ public class TestCardDelivery {
         $("[data-test-id=name].input_invalid .input__sub").shouldHave(exactText("Поле обязательно для заполнения"));
         $("[data-test-id=notification]").shouldNotBe(Condition.visible);
     }
+
     @Test
     void shouldSubmitRequestIfNameContainHyphen() {
         form.$("[data-test-id=city] input").setValue("Москва");
@@ -290,28 +306,31 @@ public class TestCardDelivery {
         form.$(".button__text").click();
         $("[data-test-id=notification]").shouldBe(Condition.visible, Duration.ofSeconds(15));
     }
-//    @Test
-//    void shouldSubmitRequestIfNameContainOnlyHyphens() {
-//        form.$("[data-test-id=city] input").setValue("Москва");
-//        form.$("[data-test-id=date] input").setValue(dateCurrentPlusThreeDays);
-//        form.$("[data-test-id=name] input").setValue("-----");
-//        form.$("[data-test-id=phone] input").setValue("+79033223322");
-//        form.$("[data-test-id=agreement]").click();
-//        form.$(".button__text").click();
-//        $("[data-test-id=name].input_invalid .input__sub").shouldHave(exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
-//        $("[data-test-id=notification]").shouldNotBe(Condition.visible);
-//    }
-//    @Test
-//    void shouldNotSubmitRequestIfNameContainOnlyOneHyphen() {
-//        form.$("[data-test-id=city] input").setValue("Москва");
-//        form.$("[data-test-id=date] input").setValue(dateCurrentPlusThreeDays);
-//        form.$("[data-test-id=name] input").setValue("-");
-//        form.$("[data-test-id=phone] input").setValue("+79033223322");
-//        form.$("[data-test-id=agreement]").click();
-//        form.$(".button__text").click();
-//        $("[data-test-id=name].input_invalid .input__sub").shouldHave(exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
-//        $("[data-test-id=notification]").shouldNotBe(Condition.visible);
-//    }
+
+    @Test
+    void shouldSubmitRequestIfNameContainOnlyHyphens() {
+        form.$("[data-test-id=city] input").setValue("Москва");
+        form.$("[data-test-id=date] input").setValue(dateCurrentPlusThreeDays);
+        form.$("[data-test-id=name] input").setValue("-----");
+        form.$("[data-test-id=phone] input").setValue("+79033223322");
+        form.$("[data-test-id=agreement]").click();
+        form.$(".button__text").click();
+        $("[data-test-id=name].input_invalid .input__sub").shouldHave(exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
+        $("[data-test-id=notification]").shouldNotBe(Condition.visible);
+    }
+
+    @Test
+    void shouldNotSubmitRequestIfNameContainOnlyOneHyphen() {
+        form.$("[data-test-id=city] input").setValue("Москва");
+        form.$("[data-test-id=date] input").setValue(dateCurrentPlusThreeDays);
+        form.$("[data-test-id=name] input").setValue("-");
+        form.$("[data-test-id=phone] input").setValue("+79033223322");
+        form.$("[data-test-id=agreement]").click();
+        form.$(".button__text").click();
+        $("[data-test-id=name].input_invalid .input__sub").shouldHave(exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
+        $("[data-test-id=notification]").shouldNotBe(Condition.visible);
+    }
+
     @Test
     void shouldNotSubmitRequestByNotIncludedCheckbox() {
         form.$("[data-test-id=city] input").setValue("Москва");
@@ -323,5 +342,5 @@ public class TestCardDelivery {
         $("[data-test-id=notification]").shouldNotBe(Condition.visible);
     }
 
-    
+
 }
